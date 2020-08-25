1c2b6a025478c6a1909610a47aa7e0db2bfb2d0d
Anni-HSE <55951421+Anni-HSE@users.noreply.github.com>
2020-08-26 00:13:44 +0500

    GEE

--- HelloBot2.0/Program.cs
+++ HelloBot2.0/Program.cs
@@ -16,7 +16,7 @@ namespace HelloBot2._0
             CreateHostBuilder(args).Build().Run();
 
 
-
+            // gjgjg
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
