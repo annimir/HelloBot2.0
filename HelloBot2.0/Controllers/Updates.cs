6d52552e6eada7998850fb0eb64f4060af2187c7
Anni-HSE <55951421+Anni-HSE@users.noreply.github.com>
2020-08-25 23:20:18 +0500

    Добавьте файлы проекта.

---
+++ HelloBot2.0/Controllers/Updates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HelloBot2._0.Controllers
+{
+    public class Updates
+    {
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Объект, инициировавший событие
+        /// Структура объекта зависит от типа уведомления
+        /// </summary>
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+
+        /// <summary>
+        /// ID сообщества, в котором произошло событие
+        /// </summary>
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+    }
+}
